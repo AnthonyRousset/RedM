3467fe9c3868b33b7b0ef68cb6e188cd2a6085b2
kaloutch <anthony.rousset.1989@gmail.com>
2025-03-22 18:47:48 +0100

    'update'

--- html/src/hud/Slots.vue
+++ html/src/hud/Slots.vue
@@ -21,18 +21,19 @@ import { ref, onMounted } from 'vue';
 
 const quickSlots = ref([
   { "label": "Lancaster", "icon": "weapon" },
-  null,
-  null,
-  null
+  { "label": "Lancaster", "icon": "weapon1" },
+  { "label": "Lancaster", "icon": "weapon2" },
+  { "label": "Lancaster", "icon": "weapon3" },
+  { "label": "Lancaster", "icon": "weapon4" }
 ]);
 
 function getIcon(name) {
   const icons = {
     weapon: './images/weapons/lancaster.png',
-    weapon: 'images/weapons/lancaster.png',
-    weapon: '/images/weapons/lancaster.png',
-    weapon: 'public/images/weapons/lancaster.png',
-    weapon: '/public/images/weapons/lancaster.png'
+    weapon1: 'images/weapons/lancaster.png',
+    weapon2: '/images/weapons/lancaster.png',
+    weapon3: 'public/images/weapons/lancaster.png',
+    weapon4: '/public/images/weapons/lancaster.png'
   };
   return icons[name] || '❔';
 }

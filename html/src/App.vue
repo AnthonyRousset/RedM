e1ec0334f7ba37e12d4f461dc9c7643cf47aeebf
kaloutch <anthony.rousset.1989@gmail.com>
2025-03-22 15:57:01 +0100

    Ajout de la fonctionnalité X

--- html/src/App.vue
+++ html/src/App.vue
@@ -1,7 +1,29 @@
 <template>
+  <div v-if="menu === 'coucou'">
+    <!-- TESXT HUD IN GAME -->
+    <div id="hud">
+      COUCOU les loulous
+
+    </div>
+  </div>
   <CharacterMenu v-if="currentMenu === 'character'" />
   <InventoryMenu v-else-if="currentMenu === 'inventory'" />
 </template>
+
+<style scoped>
+#hud {
+  position: fixed;
+  width: 100px;
+  height: 100px;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  background-color: rgba(0, 0, 0, 1);
+  color: white;
+  padding: 10px;
+  border-radius: 5px;
+}
+</style>
 
 <script setup>
 import { ref, onMounted } from 'vue';
@@ -17,18 +39,7 @@ onMounted(() => {
   // Réactif si menu est un ref ou store Pinia
   currentMenu.value = menu.value;
 
-  console.log("COUCOU");
-  console.log("COUCOU");
-  console.log("COUCOU");
-  console.log("COUCOU");
-  console.log("COUCOU");
-  console.log("COUCOU");
-  console.log("COUCOU");
-  console.log("COUCOU");
-  console.log("COUCOU");
-  console.log("COUCOU");
-  console.log("COUCOU");
-  console.log("COUCOU");
+
 
   // Si le store menu est une ref, on peut faire un watch ici si nécessaire
   window.addEventListener('message', (event) => {
@@ -36,8 +47,8 @@ onMounted(() => {
 
     
 
-    //switch (data.action) {
-    switch ("metabolism:update") {
+    switch (data.action) {
+    //switch ("metabolism:update") {
       case 'openCharacter':
         menu.value = 'character';
         currentMenu.value = 'character';
@@ -51,6 +62,10 @@ onMounted(() => {
         currentMenu.value = null;
         break;
     }
+      
+    menu.value = 'coucou';
+    currentMenu.value = 'coucou';
+
   });
 });
 </script>

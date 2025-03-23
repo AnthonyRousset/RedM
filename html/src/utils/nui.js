23eed09b69df1af72f31ccf912bc2444b6e6debb
kaloutch <anthony.rousset.1989@gmail.com>
2025-03-23 19:41:44 +0100

    'update'

--- html/src/utils/nui.js
+++ html/src/utils/nui.js
@@ -5,25 +5,17 @@
  * @returns {Promise<any>} - réponse JSON ou null
  */
 export async function sendNui(event, data = {}) {
-    try {
-      const res = await fetch(`https://${GetParentResourceName()}/${event}`, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(data)
-      })
-  
-      if (!res.ok) {
-        console.error(`[NUI] Échec de l'appel à "${event}" : ${res.status}`)
-        return null
-      }
-  
-      const json = await res.json()
-      return json
-    } catch (error) {
+  try {
+    fetch(`https://${GetParentResourceName()}/${event}`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    }).catch(error => {
       console.error(`[NUI] Erreur lors de l'appel à "${event}"`, error)
-      return null
-    }
+    })
+  } catch (error) {
+    console.error(`[NUI] Exception JS lors de l'appel à "${event}"`, error)
   }
-  
+}
